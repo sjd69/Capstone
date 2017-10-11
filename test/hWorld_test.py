0ec9fbf45626df20656c429ff135a986bcbd6398
job96 <job96@pitt.edu>
2017-10-11 08:01:11 -0400

    HI MARTIN :)

--- test/hWorld_test.py
+++ test/hWorld_test.py
@@ -3,12 +3,9 @@ from hWorld import hello
 
 
 class testing(unittest.TestCase):
-    
-    def test_string(self):
-        arr = ['Jordan','Heidi','Alex','Mama','Tyree','Tim','Paul']
-        for x in arr:
-            print(x)
-            self.assertEqual(hello(x), 'Hello World: '+x)
-
+	def test_case(self):
+		test1 = hello("Heidi")
+		self.assertEqual(test1, 'Hello World: Heidi')
+		
 if __name__ == '__main__':
     unittest.main()
