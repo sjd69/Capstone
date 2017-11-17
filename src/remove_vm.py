f54ca4e58d25c589aad7acf814af70d07beedcaa
JaebeomWFP <sjd69@pitt.edu>
2017-11-16 23:52:53 -0500

    initial

--- src/remove_vm.py
+++ src/remove_vm.py
@@ -1,11 +1,7 @@
-from pyVim import connect
-from pyVmomi import vmodl
-from pyVmomi import vim
-from pyVmomi import vmodl
-import atexit
-import subprocess
-from tools import tasks
+import connect
 import tools.cli as cli
+from pyVmomi import vim
+from tools import tasks
 
 
 def get_args():
@@ -32,3 +28,59 @@ def get_args():
     args = parser.parse_args()
 
     return cli.prompt_for_password(args)
+
+def get_obj(content, type, name):
+    """Create container view and search for object in it"""
+    obj = None
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, type, True)
+    for c in container.view:
+        if name:
+            if c.name == name:
+                obj = c
+                break
+        else:
+            obj = c
+            break
+
+    container.Destroy()
+    return obj
+
+
+def main():
+    args = get_args()
+
+    service_instance = connect.connect_no_ssl(args.host, args.user, args.password, args.port)
+
+    virtual_machine = None
+
+    if args.uuid:
+        virtual_machine = service_instance.content.searchIndex.FindByUuid(None, args.uuid,
+                                                                          True,
+                                                                          False)
+    elif args.name:
+        virtual_machine = get_obj(service_instance.content, [vim.VirtualMachine], args.name)
+    elif args.ip:
+        virtual_machine = service_instance.content.searchIndex.FindByIp(None, args.ip, True)
+
+    if virtual_machine is None:
+        raise SystemExit("Unable to locate VirtualMachine. Arguments given: "
+                         "vm - {0} , uuid - {1} , name - {2} , ip - {3}"
+                         .format(args.name, args.uuid, args.name, args.ip))
+
+    print("Found: {0}".format(virtual_machine.name))
+    print("The current powerState is: {0}".format(virtual_machine.runtime.powerState))
+    if format(virtual_machine.runtime.powerState) == "poweredOn":
+        print("Attempting to power off {0}".format(virtual_machine.name))
+        task = virtual_machine.PowerOffVM_Task()
+        tasks.wait_for_tasks(service_instance, [task])
+        print("{0}".format(task.info.state))
+
+    print("Destroying VM")
+    task = virtual_machine.Destroy_Task()
+    tasks.wait_for_tasks(service_instance, [task])
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
