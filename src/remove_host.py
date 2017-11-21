8005395304612add2adb785dce37561f2c872ac7
job96 <job96@pitt.edu>
2017-11-21 07:49:43 -0500

    fixing remove function

---
+++ src/remove_host.py
@@ -0,0 +1,45 @@
+#hello
+from pyVim import connect
+from pyVmomi import vim
+from pyVmomi import vmodl
+import atexit
+from create_vm import Vcenter
+
+def remove_host(host):
+	#TODO: check to see if cluster exists
+	#if cluster exists, check for existing host by specified name
+	#if exists, destroy; else, print "does not exist" exit
+	host_name = host.name
+	# ESXi Host enters Maintenance Mode
+	host.EnterMaintenanceMode(timeout=0, evacuatePoweredOffVms=True, maintenanceSpec=None)
+	while not host.runtime.inMaintenanceMode:
+		time.sleep(1)
+	print("ESXi Host '%s' entered in Maintenance Mode successfully!" % host_name)
+	
+	#print(host.config)
+	# Destroy ESXi Host
+	task_destroyhost = host.Destroy()
+	print("never makes it here")
+	while task_destroyhost.info.state == vim.TaskInfo.State.running:
+		time.sleep(1)
+	if task_destroyhost.info.state != vim.TaskInfo.State.success:
+		raise SystemExit("ABORT: ESXi Host '%s' failed to be removed from the vC inventory" % host_name)
+	print("ESXi Host '%s' removed from the vC inventory!" % host_name)
+
+def main():
+		service_instance = connect.SmartConnectNoSSL(host='127.0.0.1',user='user',pwd='pass',port=8989)
+		
+		container = service_instance.RetrieveContent()
+		host_view = container.viewManager.CreateContainerView(container.rootFolder,[vim.HostSystem],True)
+		
+		for host in host_view.view:
+			#print(CR.Destroy())
+			if host.name == "127.0.0.1":
+				#remove_host(host)
+				host.Destroy_Task()
+				break
+			
+	
+# Start program
+if __name__ == "__main__":
+    main()
