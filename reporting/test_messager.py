5a54d2c8a09850ba6a14931b1d2870fd06a0ae44
Martin Kennedy <hurricos@gmail.com>
2017-11-21 14:33:42 -0500

    Updated test messagers to carry links
    .. and, via commit, testing entire CI server

--- reporting/test_messager.py
+++ reporting/test_messager.py
@@ -1,9 +1,7 @@
 import subprocess
 
 with open('test_message.json', 'r') as msg_file:
-    payload = msg_file.read().replace('\n', '')
-    
-# payload = '{"text":"TESTING, <@U751W3GQJ>", "username":"CI Testbot", "channel":"@U751W3GQJ"}'
+    payload = msg_file.read().replace('\n', '').replace('COMMIT_HASH', sys.argv[1])
 
 webhook_url = "https://hooks.slack.com/services/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"
 
